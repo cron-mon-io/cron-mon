af0fc1ae2d77144a3896285aae29393ec90a7644
Howard Smith <hj_smith@live.com>
2024-04-01 21:48:00 +0100

    chore: Remove default catcher
    Need to work on this later - was being used for _all_ errors.

--- api/src/main.rs
+++ api/src/main.rs
@@ -7,24 +7,6 @@ pub mod infrastructure;
 
 use crate::application::routes;
 use rocket::fs::FileServer;
-use rocket::http::Status;
-use rocket::serde::json::Json;
-use rocket::Request;
-use serde::Serialize;
-
-#[derive(Serialize)]
-struct ErrorResponse {
-    status: String,
-    message: String,
-}
-
-#[catch(default)]
-fn default(_status: Status, _req: &Request) -> Json<ErrorResponse> {
-    return Json(ErrorResponse {
-        status: "error".to_owned(),
-        message: "Oh dear".to_owned(),
-    });
-}
 
 #[launch]
 fn rocket() -> _ {
@@ -34,5 +16,4 @@ fn rocket() -> _ {
             routes![routes::health::health, routes::list_monitors::list_monitors],
         )
         .mount("/api/v1/docs", FileServer::from("/usr/cron-mon/api/docs"))
-        .register("/", catchers![default])
 }
