d69c035814553d21f83ac3d01678b9a717310ac4
Howard Smith <hj_smith@live.com>
2024-05-05 22:46:13 +0100

    chore: Add Vuetify and Material Design Icons

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,11 +1,27 @@
-import './assets/main.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 import { createApp } from 'vue'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    }
+  }
+})
 
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(router).use(vuetify).mount('#app')
