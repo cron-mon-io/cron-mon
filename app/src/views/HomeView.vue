961b81edf59fa8179ac6ef126074283cdf436497
Howard Smith <hj_smith@live.com>
2024-05-07 22:30:08 +0100

    chore: Remove example Vue x Vite stuff

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>This is the home page</h1>
+  </div>
 </template>
