1ec74d2009395bce7e4d0a6e030793e01be08cd0
Howard Smith <hj_smith@live.com>
2024-05-11 22:19:55 +0100

    feat: Monitors view

--- app/src/views/MonitorsView.vue
+++ app/src/views/MonitorsView.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <h1>This the monitors view</h1>
+  <div class="d-flex flex-column align-center">
+    <MonitorInfo v-for="monitor in monitors" :key="monitor.monitor_id" :monitor="monitor" />
   </div>
 </template>
+
+<script setup lang="ts">
+import MonitorInfo from '@/components/MonitorInfo.vue'
+import { MonitorRepository } from '@/repos/monitor-repo'
+import { ref } from 'vue'
+
+const monitorRepo = new MonitorRepository()
+const monitors = ref(await monitorRepo.getMonitorInfos())
+</script>
