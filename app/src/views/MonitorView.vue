b6f210af498b9ebafbfd77a1463b13b9b85f8a6f
Howard Smith <hj_smith@live.com>
2024-05-16 22:40:28 +0100

    feat: Modify existing monitors

--- app/src/views/MonitorView.vue
+++ app/src/views/MonitorView.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card class="elevation-2 mx-6 mt-13">
-    <MonitorSummary :monitor="monitor" :is-new="false" />
+    <MonitorSummary :monitor="monitor" :is-new="$cookies.isKey(monitor.monitor_id)" />
     <v-card-text>
       <v-chip
         append-icon="mdi-content-copy"
@@ -15,6 +15,10 @@
           You'll need this to use the monitor in your cron job, see the docs for more.
         </v-tooltip>
       </v-chip>
+      <v-btn append-icon="mdi-pencil" color="primary" class="ma-3" @click="openEditDialog">
+        Edit Monitor
+        <v-tooltip activator="parent" location="top">Click to modify this Monitor</v-tooltip>
+      </v-btn>
       <!--
         When the key changes Vue will re-render the component so using `late` and `succeeded`
         in the key means that as jobs become late or are finished we'll trigger a re-render.
@@ -27,25 +31,55 @@
       />
     </v-card-text>
   </v-card>
+  <SetupMonitorDialog
+    :dialogActive="editDialogActive"
+    @dialog-complete="editDialogComplete"
+    @dialog-aborted="closeEditDialog"
+    :monitor="monitor"
+  />
 </template>
 
 <script setup lang="ts">
+import { ref, inject } from 'vue'
+import type { VueCookies } from 'vue-cookies'
 import { useRoute } from 'vue-router'
-import { ref } from 'vue'
 
-import { MonitorRepository } from '@/repos/monitor-repo'
 import JobInfo from '@/components/JobInfo.vue'
 import MonitorSummary from '@/components/MonitorSummary.vue'
+import SetupMonitorDialog from '@/components/SetupMonitorDialog.vue'
+import type { BasicMonitorInformation, MonitorInformation } from '@/models/monitor'
+import { MonitorRepository } from '@/repos/monitor-repo'
+import router from '@/router'
 
 const ONE_MINUTE_MS = 60 * 1000
 
 const route = useRoute()
+const cookies = inject<VueCookies>('$cookies')
 
 const monitorRepo = new MonitorRepository()
 const monitor = ref(await monitorRepo.getMonitor(route.params.id as string))
+const editDialogActive = ref(false)
 
 function copyMonitorId() {
   navigator.clipboard.writeText(monitor.value.monitor_id)
+}
+
+async function editDialogComplete(monitorInfo: BasicMonitorInformation) {
+  const newMonitor = {
+    monitor_id: monitor.value.monitor_id,
+    ...monitorInfo
+  } as MonitorInformation
+  monitor.value = await monitorRepo.updateMonitor(newMonitor)
+  cookies?.set(monitor.value.monitor_id, 'new', '5m')
+  closeEditDialog()
+}
+
+function openEditDialog() {
+  editDialogActive.value = true
+}
+
+function closeEditDialog() {
+  editDialogActive.value = false
 }
 
 function resyncMonitor() {
