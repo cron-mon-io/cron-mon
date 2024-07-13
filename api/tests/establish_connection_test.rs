6440e2ae64dc928fadd350b868b7165d3f2e2cd5
Howard Smith <hj_smith@live.com>
2024-07-13 21:24:42 +0100

    fix: Add missing test coverage

---
+++ api/tests/establish_connection_test.rs
@@ -0,0 +1,26 @@
+use std::env;
+
+use tokio::test;
+
+use cron_mon_api::{errors::AppError, infrastructure::database::establish_connection};
+
+#[test]
+async fn test_establish_connection() {
+    let conn_result = establish_connection().await;
+    assert!(conn_result.is_ok());
+}
+
+#[test]
+async fn test_establish_connection_error() {
+    env::set_var(
+        "DATABASE_URL",
+        "postgres://postgres:password@localhost:5432/monitors_test",
+    );
+
+    let conn = establish_connection().await;
+    assert!(conn.is_err());
+    assert!(matches!(
+        conn.err().unwrap(),
+        AppError::RepositoryError { .. }
+    ));
+}
