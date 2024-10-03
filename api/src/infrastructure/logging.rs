f46eca68ddb0ba84c75dec07a98159b30af6b89a
Howard Smith <hj_smith@live.com>
2024-10-03 22:35:15 +0100

    chore: Remove useless coverage comments

--- api/src/infrastructure/logging.rs
+++ api/src/infrastructure/logging.rs
@@ -2,9 +2,6 @@ use std::env;
 
 use tracing_subscriber;
 
-// #[coverage(off)] We need to comment this out for now as this feature isn't stable yet.
-// See https://github.com/rust-lang/rust/issues/84605
-// Not really sure how to test this, so we'll just ignore it for now.
 pub fn init_logging() {
     let json_logging = match env::var("JSON_LOGGING") {
         Ok(val) => val.trim().parse().unwrap(),
@@ -17,4 +14,3 @@ pub fn init_logging() {
         builder.init();
     }
 }
-// #[coverage(on)]
