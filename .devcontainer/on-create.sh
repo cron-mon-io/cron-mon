cc39c4986fcbb3ef812b15251e7e9de7f32b12bf
Howard Smith <hj_smith@live.com>
2024-10-09 20:03:48 +0000

    feat: Setup Git, bash completions, and starship.rs prompt in devcontainer

---
+++ .devcontainer/on-create.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+
+# Need bash-completion for bash completion, curl to download startship.rs, and ssh for Git.
+apt-get update && apt-get install -y bash-completion curl ssh
+
+# Setup bash completion
+echo "source /etc/bash_completion" >> ~/.bashrc
+
+# Setup starship prompt
+curl -sS https://starship.rs/install.sh | sh -s -- -y
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
