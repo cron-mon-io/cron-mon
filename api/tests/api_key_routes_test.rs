b2e84041319fbcb0379e84ea6df869ca57b3f1dc
Howard Smith <hj_smith@live.com>
2024-10-13 20:40:16 +0000

    test: Add integration tests for API key auth error handling

---
+++ api/tests/api_key_routes_test.rs
@@ -0,0 +1,77 @@
+pub mod common;
+
+use pretty_assertions::assert_eq;
+use rocket::http::{ContentType, Header, Status};
+use rocket::local::asynchronous::LocalResponse;
+use serde_json::{json, Value};
+
+use common::get_test_client;
+
+#[tokio::test]
+async fn test_start_job_with_no_api_key() {
+    let (_mock_server, client) = get_test_client("test-kid", false).await;
+
+    let response = client
+        .post("/api/v1/monitors/c1bf0515-df39-448b-aa95-686360a33b36/jobs/start")
+        .dispatch()
+        .await;
+
+    assert_response_is_unauthorized(response, "The request requires user authentication.").await;
+}
+
+#[tokio::test]
+async fn test_start_job_with_invalid_api_key() {
+    let (_mock_server, client) = get_test_client("test-kid", false).await;
+
+    let response = client
+        .post("/api/v1/monitors/c1bf0515-df39-448b-aa95-686360a33b36/jobs/start")
+        .header(Header::new("X-API-Key", "invalid-key"))
+        .dispatch()
+        .await;
+
+    assert_response_is_unauthorized(response, "Unauthorized: Invalid API key").await;
+}
+
+#[tokio::test]
+async fn test_finish_job_with_no_api_key() {
+    let (_mock_server, client) = get_test_client("test-kid", false).await;
+
+    let response = client
+        .post("/api/v1/monitors/c1bf0515-df39-448b-aa95-686360a33b36/jobs/9d4e2d69-af63-4c1e-8639-60cb2683aee5/finish")
+        .json(&json!({"succeeded": true, "output": "Test output"}))
+        .dispatch()
+        .await;
+
+    assert_response_is_unauthorized(response, "The request requires user authentication.").await;
+}
+
+#[tokio::test]
+async fn test_finish_job_with_invalid_api_key() {
+    let (_mock_server, client) = get_test_client("test-kid", false).await;
+
+    let response = client
+        .post("/api/v1/monitors/c1bf0515-df39-448b-aa95-686360a33b36/jobs/9d4e2d69-af63-4c1e-8639-60cb2683aee5/finish")
+        .header(Header::new("X-API-Key", "invalid-key"))
+        .json(&json!({"succeeded": true, "output": "Test output"}))
+        .dispatch()
+        .await;
+
+    assert_response_is_unauthorized(response, "Unauthorized: Invalid API key").await;
+}
+
+async fn assert_response_is_unauthorized(response: LocalResponse<'_>, description: &str) {
+    assert_eq!(response.status(), Status::Unauthorized);
+    assert_eq!(response.content_type(), Some(ContentType::JSON));
+
+    let response_body = response.into_json::<Value>().await.unwrap();
+    assert_eq!(
+        response_body,
+        json!({
+            "error": {
+                "code": 401,
+                "description": description,
+                "reason": "Unauthorized"
+            }
+        })
+    );
+}
