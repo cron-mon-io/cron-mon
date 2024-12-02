aa8956a37aff549d8bab2b0e3f79b625bcadbaeb
Howard Smith <hj_smith@live.com>
2024-12-02 21:28:29 +0000

    feat: Add migration to setup alert config tables

--- api/src/infrastructure/db_schema.rs
+++ api/src/infrastructure/db_schema.rs
@@ -1,6 +1,21 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    alert_config (alert_config_id) {
+        alert_config_id -> Uuid,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+        name -> Varchar,
+        tenant -> Varchar,
+        #[sql_name = "type"]
+        type_ -> Varchar,
+        active -> Bool,
+        on_late -> Bool,
+        on_error -> Bool,
+    }
+}
+
+diesel::table! {
     api_key (api_key_id) {
         api_key_id -> Uuid,
         created_at -> Timestamp,
@@ -43,6 +58,33 @@ diesel::table! {
     }
 }
 
-diesel::joinable!(job -> monitor (monitor_id));
+diesel::table! {
+    monitor_alert_config (alert_config_id, monitor_id) {
+        alert_config_id -> Uuid,
+        monitor_id -> Uuid,
+    }
+}
 
-diesel::allow_tables_to_appear_in_same_query!(api_key, job, monitor,);
+diesel::table! {
+    slack_alert_config (alert_config_id) {
+        alert_config_id -> Uuid,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+        slack_channel -> Varchar,
+        slack_bot_oauth_token -> Varchar,
+    }
+}
+
+diesel::joinable!(job -> monitor (monitor_id));
+diesel::joinable!(monitor_alert_config -> alert_config (alert_config_id));
+diesel::joinable!(monitor_alert_config -> monitor (monitor_id));
+diesel::joinable!(slack_alert_config -> alert_config (alert_config_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    alert_config,
+    api_key,
+    job,
+    monitor,
+    monitor_alert_config,
+    slack_alert_config,
+);
