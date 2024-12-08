800f29cd851b3b4fc45222c450ba2aadd3adc20a
Howard Smith <hj_smith@live.com>
2024-12-08 22:48:50 +0000

    Feat/alert configs for monitors (#93)
    * feat: Add `Error` for alert configuration errors
    
    * feat: Add list of monitor IDs to `AlertConfig` and functionality to assocciate `Monitor`s with `AlertConfig`s
    
    * feat: Support monitor IDs in `AlertConfig` data models
    
    * chore: Add missing `check_for_backend(Pg)` to alert config data models
    
    * feat: Retrieve `monitor_alert_config` data and populate `AlertConfig` models in `.get` and `.all`
    
    * feat: Include `monitor_alert_config` rows in `.save`
    
    * test: Add seed data for `monitor_alert_config`
    
    * chore: Add TODO comment
    
    * chore: Fix instances of `monitor` in `AlertConfigRepository` integration tests
    
    * test: Cover `AlertConfig.monitor_ids` in `AlertConfigRepository` integration tests
    
    * feat: Add trait and method for retrieving `AlertConfig`s by monitors that use them
    
    * test: Add coverage for `get_by_monitors`
    
    * fix: Need to use `distinct` to avoid duplicates
    
    * refactor: Remove duplication between `get_by_monitors` and `all`

---
+++ api/src/infrastructure/repositories/alert_configs.rs
@@ -0,0 +1,18 @@
+use async_trait::async_trait;
+use uuid::Uuid;
+
+#[cfg(test)]
+use mockall::automock;
+
+use crate::domain::models::alert_config::AlertConfig;
+use crate::errors::Error;
+
+#[cfg_attr(test, automock)]
+#[async_trait]
+pub trait GetByMonitors {
+    async fn get_by_monitors(
+        &mut self,
+        monitor_ids: &[Uuid],
+        tenant: &str,
+    ) -> Result<Vec<AlertConfig>, Error>;
+}
