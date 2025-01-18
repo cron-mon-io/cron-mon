21d5f9307d935d8b355da3d321a97775e7ae3900
Howard Smith <hj_smith@live.com>
2025-01-18 22:58:43 +0000

    refactor: `Repo` -> `MonitorRepo` in `ProcessLateJobsService`

--- api/src/application/services/monitors/process_late_jobs.rs
+++ api/src/application/services/monitors/process_late_jobs.rs
@@ -3,26 +3,31 @@ use tracing::info;
 use crate::domain::models::Monitor;
 use crate::errors::Error;
 use crate::infrastructure::notify::NotifyLateJob;
-use crate::infrastructure::repositories::{monitor::GetWithLateJobs, Repository};
+use crate::infrastructure::repositories::{
+    alert_config::GetByMonitors, monitor::GetWithLateJobs, Repository,
+};
 
 pub struct ProcessLateJobsService<
-    Repo: GetWithLateJobs + Repository<Monitor>,
+    MonitorRepo: GetWithLateJobs + Repository<Monitor>,
     Notifier: NotifyLateJob,
 > {
-    repo: Repo,
+    monitor_repo: MonitorRepo,
     notifier: Notifier,
 }
 
-impl<Repo: GetWithLateJobs + Repository<Monitor>, Notifier: NotifyLateJob>
-    ProcessLateJobsService<Repo, Notifier>
+impl<MonitorRepo: GetWithLateJobs + Repository<Monitor>, Notifier: NotifyLateJob>
+    ProcessLateJobsService<MonitorRepo, Notifier>
 {
-    pub fn new(repo: Repo, notifier: Notifier) -> Self {
-        Self { repo, notifier }
+    pub fn new(monitor_repo: MonitorRepo, notifier: Notifier) -> Self {
+        Self {
+            monitor_repo,
+            notifier,
+        }
     }
 
     pub async fn process_late_jobs(&mut self) -> Result<(), Error> {
         info!("Beginning check for late Jobs...");
-        let mut monitors_with_late_jobs = self.repo.get_with_late_jobs().await?;
+        let mut monitors_with_late_jobs = self.monitor_repo.get_with_late_jobs().await?;
 
         for mon in monitors_with_late_jobs.as_mut_slice() {
             let monitor_name = mon.name.clone();
@@ -36,7 +41,7 @@ impl<Repo: GetWithLateJobs + Repository<Monitor>, Notifier: NotifyLateJob>
                 }
             }
 
-            self.repo.save(mon).await?;
+            self.monitor_repo.save(mon).await?;
         }
 
         info!("Check for late Jobs complete");
@@ -79,64 +84,70 @@ mod tests {
     #[traced_test]
     #[tokio::test(start_paused = true)]
     async fn test_process_late_jobs_service() {
-        let mut mock_repo = MockMonitorRepo::new();
-        mock_repo.expect_get_with_late_jobs().once().returning(|| {
-            Ok(vec![
-                Monitor {
-                    monitor_id: gen_uuid("41ebffb4-a188-48e9-8ec1-61380085cde3"),
-                    tenant: "foo-tenant".to_owned(),
-                    name: "background-task.sh".to_owned(),
-                    expected_duration: 300,
-                    grace_duration: 100,
-                    jobs: vec![
-                        Job {
-                            job_id: gen_uuid("01a92c6c-6803-409d-b675-022fff62575a"),
-                            start_time: gen_relative_datetime(-500),
-                            max_end_time: gen_relative_datetime(-100),
+        let mut mock_monitor_repo = MockMonitorRepo::new();
+        mock_monitor_repo
+            .expect_get_with_late_jobs()
+            .once()
+            .returning(|| {
+                Ok(vec![
+                    Monitor {
+                        monitor_id: gen_uuid("41ebffb4-a188-48e9-8ec1-61380085cde3"),
+                        tenant: "foo-tenant".to_owned(),
+                        name: "background-task.sh".to_owned(),
+                        expected_duration: 300,
+                        grace_duration: 100,
+                        jobs: vec![
+                            Job {
+                                job_id: gen_uuid("01a92c6c-6803-409d-b675-022fff62575a"),
+                                start_time: gen_relative_datetime(-500),
+                                max_end_time: gen_relative_datetime(-100),
+                                end_state: None,
+                                late_alert_sent: false,
+                                error_alert_sent: false,
+                            },
+                            Job {
+                                job_id: gen_uuid("3b9f5a89-ebc2-49bf-a9dd-61f52f7a3fa0"),
+                                start_time: gen_relative_datetime(-1000),
+                                max_end_time: gen_relative_datetime(-600),
+                                end_state: Some(EndState {
+                                    end_time: gen_relative_datetime(-550),
+                                    succeeded: true,
+                                    output: None,
+                                }),
+                                late_alert_sent: false,
+                                error_alert_sent: false,
+                            },
+                            Job {
+                                job_id: gen_uuid("051c2f13-20ae-456c-922b-b5799689d4ff"),
+                                start_time: gen_relative_datetime(0),
+                                max_end_time: gen_relative_datetime(400),
+                                end_state: None,
+                                late_alert_sent: false,
+                                error_alert_sent: false,
+                            },
+                        ],
+                    },
+                    Monitor {
+                        monitor_id: gen_uuid("841bdefb-e45c-4361-a8cb-8d247f4a088b"),
+                        tenant: "bar-tenant".to_owned(),
+                        name: "get-pending-orders | generate invoices".to_owned(),
+                        expected_duration: 21_600,
+                        grace_duration: 1_800,
+                        jobs: vec![Job {
+                            job_id: gen_uuid("9d90c314-5120-400e-bf03-e6363689f985"),
+                            start_time: gen_relative_datetime(-30_000),
+                            max_end_time: gen_relative_datetime(-6_600),
                             end_state: None,
                             late_alert_sent: false,
                             error_alert_sent: false,
-                        },
-                        Job {
-                            job_id: gen_uuid("3b9f5a89-ebc2-49bf-a9dd-61f52f7a3fa0"),
-                            start_time: gen_relative_datetime(-1000),
-                            max_end_time: gen_relative_datetime(-600),
-                            end_state: Some(EndState {
-                                end_time: gen_relative_datetime(-550),
-                                succeeded: true,
-                                output: None,
-                            }),
-                            late_alert_sent: false,
-                            error_alert_sent: false,
-                        },
-                        Job {
-                            job_id: gen_uuid("051c2f13-20ae-456c-922b-b5799689d4ff"),
-                            start_time: gen_relative_datetime(0),
-                            max_end_time: gen_relative_datetime(400),
-                            end_state: None,
-                            late_alert_sent: false,
-                            error_alert_sent: false,
-                        },
-                    ],
-                },
-                Monitor {
-                    monitor_id: gen_uuid("841bdefb-e45c-4361-a8cb-8d247f4a088b"),
-                    tenant: "bar-tenant".to_owned(),
-                    name: "get-pending-orders | generate invoices".to_owned(),
-                    expected_duration: 21_600,
-                    grace_duration: 1_800,
-                    jobs: vec![Job {
-                        job_id: gen_uuid("9d90c314-5120-400e-bf03-e6363689f985"),
-                        start_time: gen_relative_datetime(-30_000),
-                        max_end_time: gen_relative_datetime(-6_600),
-                        end_state: None,
-                        late_alert_sent: false,
-                        error_alert_sent: false,
-                    }],
-                },
-            ])
-        });
-        mock_repo.expect_save().times(2).returning(|_| Ok(()));
+                        }],
+                    },
+                ])
+            });
+        mock_monitor_repo
+            .expect_save()
+            .times(2)
+            .returning(|_| Ok(()));
 
         let mut mock_notifier = MockNotifyLateJob::new();
         mock_notifier
@@ -167,7 +178,7 @@ mod tests {
             })
             .returning(|_, _, _| Ok(()));
 
-        let mut service = ProcessLateJobsService::new(mock_repo, mock_notifier);
+        let mut service = ProcessLateJobsService::new(mock_monitor_repo, mock_notifier);
 
         let result = service.process_late_jobs().await;
         assert!(result.is_ok());
